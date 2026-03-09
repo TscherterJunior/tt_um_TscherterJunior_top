dc24c85e6c9e731303d39d6e2eda7ec495b7c045
Nicolas <nicolas@tscherter.net>
2026-03-09 12:01:44 +0100

    added alu

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_TcherterJunior_top (
+module tt_um_TscherterJunior_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,41 @@ module tt_um_TcherterJunior_top (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  // input wires renamed
+  wire [7:0] data_i;
+
+  // output wires renamed
+  wire read_en_o;
+  wire instr_en_o;
+  wire [7:0] addr_o;
+  wire [7:0] data_o;
+
+
+  //regs
+  reg [7:0] reg0;
+  reg [7:0] reg1;
+  reg [7:0] reg2;
+  reg [7:0] reg3;
+  reg [7:0] reg4;
+  reg [7:0] reg5;
+  reg [7:0] reg6;
+  reg [7:0] reg7;
+
+  tt_um_TscherterJunior_ALU ALU_test (
+    .opcode_i(4'b0010),
+
+    .acc_i(ui_in),    
+    .operand_i(uio_in), 
+
+    .acc_o(uo_out),  
+    .flags_o(data_o[1:0])
+  );
+
+  /*
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
+  */
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
---
+++ src/tt_um_TscherterJunior_ALU.v
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_TscherterJunior_ALU (
+    input wire [3:0] opcode_i,
+
+    input  wire [7:0] acc_i,    
+    input wire [7:0] operand_i, 
+
+    output reg [7:0] acc_o,  
+    output reg [1:0] flags_o
+
+);
+
+    reg [8:0] tempres;
+
+    //enum bit[] {ADD = 4b'0010, SUB = 4b'0011, XOR = 4b'0111, AND = 4b'1100, OR = 4b'}
+
+    always @(*) begin
+        
+        acc_o = 0;
+        flags_o = 0;
+
+        case (opcode_i)
+            4'b0010 : begin // ADD
+                tempres = acc_i + operand_i;
+                acc_o = tempres[7:0];
+                flags_o[1] = tempres[8]; // carry
+                flags_o[0] = (acc_i[7] == operand_i[7]) && (acc_i[7] != tempres[7]);
+
+            end
+            4'b0011 : begin // SUB
+                tempres = acc_i - operand_i;
+                acc_o = tempres[7:0];
+                flags_o[1] = tempres[8]; // carry
+                flags_o[0] = (~acc_i[7] & operand_i[7] & tempres[7]) || (acc_i[7] & ~operand_i[7] & ~tempres[7]);
+
+            end
+            4'b0111 : begin // XOR
+                acc_o = acc_i ^ operand_i;
+                flags_o[0] = (acc_o == 8'b0);
+                flags_o[1] = ~^acc_o; //parity
+            end
+            4'b1100 : begin // And
+                acc_o = acc_i & operand_i;
+                flags_o[0] = (acc_o == 8'b0);
+                flags_o[1] = ~^acc_o; //parity
+            end            
+            4'b1101 : begin // OR
+                acc_o = acc_i | operand_i;
+                flags_o[0] = (acc_o == 8'b0);
+                flags_o[1] = ~^acc_o; //parity
+            end   
+            4'b1110 : begin // shift left logical
+                acc_o = acc_i << operand_i[2:0];
+                flags_o[0] = (acc_o == 8'b0);
+                flags_o[1] = acc_o[7]; //sign
+            end   
+            4'b1111 : begin // shift right logical
+                acc_o = acc_i >> operand_i[2:0];
+                flags_o[0] = (acc_o == 8'b0);
+                flags_o[1] = acc_o[7]; //sign
+            end   
+            default : begin
+                acc_o = '0;
+                flags_o = '0;
+            end
+        endcase
+
+
+    end
+
+endmodule
